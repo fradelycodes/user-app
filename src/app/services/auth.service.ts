3f5e6d42c8d6e59ba35434388d189ceb134de290
fradely code <fradelydventura@gmail.com>
2020-06-09 22:27:21 -0400

    add manual admin user

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -8,29 +8,72 @@ import { Http } from '@angular/http';
 export class AuthService {
   apiUser = '/users';
   listUsers = [];
-  userName = 'Robyn Frami';
-  password = 'zocmcUj27xs1b0Q';
+  userName = '';
+  password = '';
   user = {};
-  constructor(private http: Http) {}
+  isValidUser = false;
+  isValidPass = false;
+  constructor(private router: Router) {}
 
   async getUsers() {
-    try {
-      //request
-      await this.http
-        .get(environment.baseUrl + this.apiUser)
-        .subscribe((response) => {
-          this.listUsers = response.json();
-          this.login();
-        });
-    } catch (err) {
-      console.log(err);
-    }
+    this.listUsers = [
+      {
+        name: 'Fradely Ventura',
+        userName: 'fradelydventura@gmail.com',
+        password: 12345,
+        roles: 'user',
+        avatar:
+          'https://s3.amazonaws.com/uifaces/faces/twitter/nathalie_fs/128.jpg',
+      },
+      {
+        name: 'Fradely Dilone',
+        userName: 'fradely@hotmail.com',
+        password: 12345678,
+        roles: 'admin',
+        avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/SULiik/128.jpg',
+      },
+    ];
+
+    // try {
+
+    //   await this.http
+    //     .get(environment.baseUrl + this.apiUser)
+    //     .subscribe((response) => {
+    //       this.listUsers = response.json();
+    //       this.login();
+    //     });
+    // } catch (err) {
+    //   console.log(err);
+    // }
   }
   //method
   login() {
-    this.user = this.listUsers.find(
-      (u) => u.name == this.userName && u.password === this.password
+    if (this.userName == '' && this.password == '') {
+      this.isValidPass = true;
+      this.isValidUser = true;
+      return;
+    }
+    var user = this.listUsers.find(
+      (u) => u.userName == this.userName && u.password === this.password
     );
-    console.log(this.user);
+
+    if (user) {
+      return;
+    }
+    localStorage.setItem('currentUser', user);
+    var currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    console.log(currentUser);
+    this.router.navigate(['/home']);
+  }
+
+  validateUser() {
+    var currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    console.log(currentUser);
+
+    if (currentUser) {
+      this.router.navigate(['/home']);
+    } else {
+      this.router.navigate(['/']);
+    }
   }
 }

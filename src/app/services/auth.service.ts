1303399e71dd237c1ce8fc29a7a8523201457996
fradely code <fradelydventura@gmail.com>
2020-06-09 20:56:47 -0400

    add login method find 1 user

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,16 +6,31 @@ import { Http } from '@angular/http';
   providedIn: 'root',
 })
 export class AuthService {
-  apiUser = '/user';
+  apiUser = '/users';
   listUsers = [];
+  userName = 'Robyn Frami';
+  password = 'zocmcUj27xs1b0Q';
+  user = {};
   constructor(private http: Http) {}
 
   async getUsers() {
     try {
       //request
-      await this.http.get(environment.baseUrl + this.apiUser).subscribe();
+      await this.http
+        .get(environment.baseUrl + this.apiUser)
+        .subscribe((response) => {
+          this.listUsers = response.json();
+          this.login();
+        });
     } catch (err) {
       console.log(err);
     }
+  }
+  //method
+  login() {
+    this.user = this.listUsers.find(
+      (u) => u.name == this.userName && u.password === this.password
+    );
+    console.log(this.user);
   }
 }
